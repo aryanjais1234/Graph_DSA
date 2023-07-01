e5c7a13188b5e064da31e2019933ece1a6bbd3e1
Aryan Jaiswal <64827664+aryanjais1234@users.noreply.github.com>
2023-07-01 08:33:36 +0000

    Insert and Print

--- Lect_1.cpp
+++ Lect_1.cpp
@@ -1,8 +1,52 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+template <typename T >
+
+class graph{
+
+    public:
+    unordered_map<T,list<T>> adj;
+
+    void addEdge(T u, T v, bool direction){
+        //direction==0-> undirected graph
+        //direction==1-> directed graph
+
+        // create an edge from u to v
+        adj[u].push_back(v);
+
+        if(!direction){
+            // create an edge from v to u
+        adj[v].push_back(u);
+        }
+    }
+
+    void printAdjList(){
+        for(auto i:adj){
+            cout<<i.first<<"->";
+            for(auto j:i.second){
+                cout<<j<<", ";
+            }
+            cout<<endl;
+        }
+    }
+
+};
+
+
 int main(){
-    cout<<"Hello World";
-    int a;
-    cin>>a;
-    cout<<a;
+    int n;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    int m;
+    cout<<"Enter the number of edges: ";
+    cin>>m;
+
+    graph<int> g;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        g.addEdge(u,v,0);
+    }
+    g.printAdjList();
 }
